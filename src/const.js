f83b6c146bb0b62ee75edb23c02d75b640c24876
Alexander Sabanov <alexsab@mail.ru>
2024-02-19 00:13:17 +0400

    add config, images

--- src/const.js
+++ src/const.js
@@ -1,18 +1,18 @@
 export const BASE_URL = 'https://alexsab-ru.github.io/astro-json/'; // Название сайта по умолчанию
-export const SITE_URL = 'localhost'; // Название сайта по умолчанию
-export const SITE_NAME = 'Название сайта'; // Название сайта по умолчанию
-export const LEGAL_ENTITY = 'ООО «Юридическое название»'; // Юр лицо
-export const LEGAL_INN = '1234567890'; // ИНН
-export const LEGAL_CITY = 'г. Город'; // Город
-export const LEGAL_CITY_WHERE = 'Городе'; // где? в Городе
+export const SITE_URL = 'kaiyi-krd.ru'; // Название сайта по умолчанию
+export const SITE_NAME = 'Автохолдинг'; // Название сайта по умолчанию
+export const LEGAL_ENTITY = 'ООО «Автохолдинг»'; // Юр лицо
+export const LEGAL_INN = '6324066475'; // ИНН
+export const LEGAL_CITY = 'г. Краснодар'; // Город
+export const LEGAL_CITY_WHERE = 'Краснодаре'; // где? в Городе
 export const SITE_DESCR = 'Официальный дилерский центр'; // Описание сайта по умолчанию
 
-export const BRAND = 'Baic';
+export const BRAND = 'Kaiyi';
 
 export const LINKS_MENU = [
-	{url: 'cars/', name: 'Авто в наличии'},
-	{url: 'special-offers/', name: 'Спецпредложения'},
-	{url: 'news/', name: 'Новости'},
+	// {url: 'cars/', name: 'Авто в наличии'},
+	// {url: 'special-offers/', name: 'Спецпредложения'},
+	// {url: 'news/', name: 'Новости'},
 	{url: 'test-drive/', name: 'Запись на тест-драйв'},
 	{url: 'service-request/', name: 'Запись на сервис'},
 	{url: '#services', name: 'Услуги'},
@@ -22,4 +22,4 @@ export const LINKS_MENU = [
 export const HEADER_TOP_LINE = '';
 
 export const AGREE_LABEL = '<span>Я согласен на</span><a href="/privacy-policy" class="underline transition-all hover:no-underline" target="_blank">обработку персональных данных</a>';
-export const FOOTER_INFO = '<sup>*</sup> Вся представленная на сайте информация, касающаяся автомобилей и сервисного обслуживания, носит информационный характер и не является публичной офертой, определяемой положениями ст. 437 ГК РФ. Все цены, указанные на данном сайте, носят информационный характер. Для получения подробной информации просьба обращаться к менеджерам отдела продаж по номеру телефона <a href="tel:+79991234567">+7&nbsp;(999) 123-45-67</a>. Опубликованная на данном сайте информация может быть изменена в любое время без предварительного уведомления.';
+export const FOOTER_INFO = '<sup>*</sup> Вся представленная на сайте информация, касающаяся автомобилей и сервисного обслуживания, носит информационный характер и не является публичной офертой, определяемой положениями ст. 437 ГК РФ. Все цены, указанные на данном сайте, носят информационный характер. Для получения подробной информации просьба обращаться к менеджерам отдела продаж по номеру телефона <a href="tel:88619920203">8 (861) 992-02-03</a>. Опубликованная на данном сайте информация может быть изменена в любое время без предварительного уведомления.';
